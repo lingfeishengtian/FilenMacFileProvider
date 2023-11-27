ef9458619c686b1c813bef8a3664769980a72432
Hunter Han <hunter.han@gmail.com>
2023-11-26 18:30:32 -0600

    Initial Commit

---
+++ FilenMacFileProvider/FilenMacFileProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilenMacFileProviderApp.swift
+//  FilenMacFileProvider
+//
+//  Created by Hunter Han on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FilenMacFileProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
